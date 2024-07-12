dd21354096a1653c5175a6cc1bde7d4e8787e899
Karan <ksrami99@gmail.com>
2024-07-12 09:14:09 +0530

    user routes and controller

--- src/app.js
+++ src/app.js
@@ -17,14 +17,18 @@ app.use(
   })
 );
 
-app.use(
-  express.urlencoded({
-    limit: "16kb",
-  })
-);
+app.use(express.urlencoded({ extended: true }))
 
 app.use(express.static("public"));
 
 app.use(cookieParser());
+
+
+// routes import
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration
+app.use("/api/v1/users" , userRouter)
+
 
 export { app };
